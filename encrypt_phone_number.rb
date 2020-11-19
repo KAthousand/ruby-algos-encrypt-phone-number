8f3a1b951aced929fce8e5f9156eb7f3ec2a164b
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-11-19 17:23:08 -0500

    kyle thousand

--- encrypt_phone_number.rb
+++ encrypt_phone_number.rb
@@ -2,7 +2,13 @@
 # RUBY ENCRYPT PHONE NUMBER
 
 def encrypt_phone_number(str)
-
+arr = str.split("")
+arr.each_with_index do |i, idx|
+  if idx <= 6 && i != "-"
+    arr[idx] = "*"
+  end
+end
+arr.join('')
 end
 
 puts encrypt_phone_number '718-867-5309'
